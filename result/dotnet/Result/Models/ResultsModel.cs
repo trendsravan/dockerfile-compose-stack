f6affa23a2143b86a6b13db975227d730d7872af
sravan kumar <43811318+trendsravan@users.noreply.github.com>
2019-12-29 16:45:32 +0530

    Create ResultsModel.cs

---
+++ result/dotnet/Result/Models/ResultsModel.cs
@@ -0,0 +1,11 @@
+namespace Result.Models
+{
+    public class ResultsModel
+    {
+        public int OptionA { get; set; }
+
+        public int OptionB { get; set; }
+
+        public int VoteCount { get; set; }
+    }
+}
