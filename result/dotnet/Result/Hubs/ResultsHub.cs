7fd1bb9b0b6e50d635f3337312ccadef264a1116
sravan kumar <43811318+trendsravan@users.noreply.github.com>
2019-12-29 16:44:44 +0530

    Create ResultsHub.cs

---
+++ result/dotnet/Result/Hubs/ResultsHub.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace Result.Hubs
+{
+    public class ResultsHub : Hub
+    {
+        //no public methods, only used for push from PublishRTesultsTimer
+    }
+}
